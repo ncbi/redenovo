aa5d7835f64e6b7da8d9f191d1b58bab77129a56
ziynet <32200164+ziynet@users.noreply.github.com>
2026-05-08 10:33:26 -0400

    ReDeNovo v1

--- src/redenovo/exporter.py
+++ src/redenovo/exporter.py
@@ -1,5 +1,4 @@
 import os
-# get logger instance
 import logging
 _logger = logging.getLogger(__name__)
 
@@ -18,12 +17,7 @@ class Exporter(object):
 
         os.makedirs(self.outfolder, exist_ok=True)
         _logger.debug(f"Output folder {self.outfolder} is ready.")
-        # take care of the output folder
-        #try:
-        #    os.makedirs(self.outfolder)
-        #except OSError:
-        #    _logger.debug(f"Output folder {self.outfolder} exists.")
-
+    
     def write_tables(self):
         """
         Writes the optimized tensors to file
@@ -51,15 +45,20 @@ class Exporter(object):
         """
         Writes the exposure matrix to file
         """
-        _logger.info("Writing matrices to file")
 
-        A_binary = self.data.A[self.data.A < self.args.exposure_thr3] = 0
-        A_binary.to_csv(os.path.join(self.outfolder, f'A_binary_with_thr_{self.args.exposure_thr3}.txt'),
-                                        sep=self.args.delimiter,
-                                        index=True,
-                                        header=True)          
+        _logger.info("Writing matrices to file")
+    
+        A_binary = self.data.A.copy()
+        A_binary[A_binary < self.args.exposure_thr3] = 0
+    
+        A_binary.to_csv(
+            os.path.join(self.outfolder, f'A_binary_with_thr_{self.args.exposure_thr3}.txt'),
+            sep=self.args.delimiter,
+            index=True,
+            header=True
+        )
+    
                                         
-    # write optimization logs to file, usefull for plotting etc
     def write_logs(self):
         with open(os.path.join(self.outfolder, "optimization.log"), 'w') as flog:
             for log in self.data.logs:
